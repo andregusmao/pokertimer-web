184cf12b99fe2adb84185b53ca317eeb8baf84d6
André Gusmão <agusmao720919@gmail.com>
2020-02-27 23:13:01 +0000

    starting web

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'typeface-roboto';
+
+import Login from './pages/Login';
+
+ReactDOM.render(<Login />, document.getElementById('root'));
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+const Login = () => {
+    return (
+        <Typography variant="h1">Hello World</Typography>
+    )
+};
+
+export default Login;
